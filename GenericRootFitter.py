cbc7bd5038269e325114bfedd3cb1ac15a54f7fd
Sulla2012 <jackorlowskischerer@gmail.com>
2015-07-22 11:17:25 -0400

    Update GenericRootFitter.py

--- GenericRootFitter.py
+++ GenericRootFitter.py
@@ -1,15 +1,21 @@
 import ROOT
 from ROOT import *
 
-rootdir = '/cms/data18/scherer/LumiFilteredScouting/2012D/Finished/'
-fdata = TFile(rootdir+"Filtered2D.root")
-#fdata.cd("genHistos/Corrected/DalitzCuts/Moderate/")
-hist = fdata.Get("genHistos/Corrected/DalitzCuts/Moderate/Dalitz_Delta_230")
-hist.Fit("gaus")
-fitter = TVirtualFitter.GetFitter()
-for i in range(0,4):
-        print(fitter.GetParameter(i))
-fitter.GetCovarianceMatrixElement(i,j)
+cutnum = 180
+
+for cutnum <= 340:
+        rootdir = '/cms/data18/scherer/LumiFilteredScouting/2012D/Finished/'
+        fdata = TFile(rootdir+"Filtered2D.root")
+        #fdata.cd("genHistos/Corrected/DalitzCuts/Moderate/")
+        histname = 'genHistos/Corrected/DalitzCuts/Moderate/Dalitz_Delta_'+str(cutnum)
+        hist = fdata.Get(histname)
+        hist.Fit("gaus")
+        fitter = TVirtualFitter.GetFitter()
+        for i in range(0,4):
+                print(fitter.GetParameter(i))
+        fitter.GetCovarianceMatrixElement(i,j)
+        cutnum = cutnum + 10
+
 
 #to define your own function
 #expofitlin =TF1("expofitlin","expo(0) + pol1(2)")
